91639697b7358ee280fcf189579935c2756e3db7
Huy Quang Nguyen <huyquang.nguyen@thomsonreuters.com>
2017-12-27 11:34:33 +1100

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -15,9 +15,21 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Chung',
+        text: 'Hey, what is going on',
+        createdAt: 123
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+    });
+
     socket.on('disconnect', () => {
         console.log('User was disconnected');
-    })
+    });
+
+
 });
 
 server.listen(port, () => {
